794ee0423e294be64bdc969d430b03c47188dd5d
Ronel Cruz <ronel.cruz.a8@gmail.com>
2022-04-10 21:15:42 +0200

    wip

--- Program.cs
+++ Program.cs
@@ -19,21 +19,21 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/error-development");
-}
-else
-{
-    app.UseExceptionHandler("/error");
-}
+//if (app.Environment.IsDevelopment())
+//{
+//    app.UseExceptionHandler("/error-development");
+//}
+//else
+//{
+//    app.UseExceptionHandler("/error");
+//}
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+//if (app.Environment.IsDevelopment())
+//{
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+//}
 
 app.UseHttpsRedirection();
 
