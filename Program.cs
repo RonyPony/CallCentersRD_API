30680dab69f8cacb44ff4e5a755a37daac0942d1
Ronel Cruz <ronel.cruz.a8@gmail.com>
2022-04-12 02:46:00 +0200

    cors

--- Program.cs
+++ Program.cs
@@ -14,7 +14,8 @@ builder.Services.AddCors(options =>
                       policy =>
                       {
                           policy.WithOrigins("http://localhost:4200",
-                                              "https://localhost:4200");
+                                              "https://localhost:4200").AllowAnyHeader()
+                                                  .AllowAnyMethod();
                       });
 });
 
